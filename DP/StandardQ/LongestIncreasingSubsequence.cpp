68761782e9a588abe205a3f05baa5b04a2a3b941
Omprakash Choudhary <omseervi098@gmail.com>
2022-07-30 15:41:42 +0530

    modified:   DP/StandardQ/LongestIncreasingSubsequence.cpp 	modified:   DP/StandardQ/EditDistance.cpp 	modified:   DP/StandardQ/Knapsack.cpp 	modified:   DP/StandardQ/LCS.cpp 	modified:   DP/StandardQ/LootHouses.cpp 	modified:   DP/StandardQ/MinCostPath.cpp 	modified:   DP/StandardQ/MinCount.cpp 	modified:   DP/StandardQ/NoOfBalancedBTs.cpp 	modified:   DP/StandardQ/StaircaseusingDP.cpp

---
+++ DP/StandardQ/LongestIncreasingSubsequence.cpp
@@ -0,0 +1,21 @@
+/*
+Given an array with N elements, you need to find the length of the longest subsequence in the given array such that all elements of the subsequence are sorted in strictly increasing order
+Sample Input 1 :
+6
+5 4 11 1 16 8
+
+Sample Output 1 :
+3
+
+Sample Output Explanation
+Length of longest subsequence is 3 i.e. (5,11,16) or (4,11,16).
+
+Sample Input 2 :
+3
+1 2 2
+
+Sample Output 2 :
+2
+*/
+#include<bits/stdc++.h>
+using namespace std;
