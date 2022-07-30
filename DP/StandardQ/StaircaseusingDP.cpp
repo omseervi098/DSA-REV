8ad47e89aa6953635b807561d61a58a1927be6c5
Omprakash Choudhary <omseervi098@gmail.com>
2022-07-30 07:42:24 +0530

    new file:   DP/StandardQ/StaircaseusingDP.cpp 	Updated Dp

---
+++ DP/StandardQ/StaircaseusingDP.cpp
@@ -0,0 +1,12 @@
+/*
+A child runs up a staircase with 'n' steps and can hop either 1 step, 2 steps or 3 steps at a time. Implement a method to count and return all possible ways in which the child can run-up to the stairs.
+Sample Input 1:
+4
+Sample Output 1:
+7
+
+Sample Input 2:
+10
+Sample Output 2:
+274
+*/
